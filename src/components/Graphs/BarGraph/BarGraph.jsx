90530546ec99f1d0059f95eae779e4225c7df3cf
deasydoesit <darwin.rinderer@gmail.com>
2019-02-18 11:47:05 -0500

    Adding animations to higher poverty line bar graph

--- src/components/Graphs/BarGraph/BarGraph.jsx
+++ src/components/Graphs/BarGraph/BarGraph.jsx
@@ -14,17 +14,22 @@ import higherPovertyData from '../../../data/BarGraphData/EAP_higher_pov.csv';
 
 class BarGraph extends Component {
   state = {
-    higherPovertyData: null,
+    higherPovertyDisplayData: null,
+    reserveData: null,
+    years: [2002, 2006, 2010, 2014, 2018],
   };
 
   componentDidMount() {
-    Promise.all([d3.csv(higherPovertyData)]).then((files) => {
+    Promise.all([
+      d3.csv(higherPovertyData), //
+    ]).then((files) => {
       const formattedHigherPovertyData = files.map((file) => {
         return formatHigherPovertyData(file, 2002);
       });
       this.setState(
         {
-          higherPovertyData: formattedHigherPovertyData[0],
+          higherPovertyDisplayData: formattedHigherPovertyData[0],
+          reserveData: files,
         },
         () => console.log(this.state.higherPovertyData)
       );
@@ -34,21 +39,38 @@ class BarGraph extends Component {
     Stickyfill.add(elements);
   }
 
-  updateGraph = () => {};
+  updateGraph = (year) => {
+    const { reserveData } = this.state;
+    const updatedHigherPovertyData = reserveData.map((data) => {
+      return formatHigherPovertyData(data, year);
+    });
+    this.setState({
+      higherPovertyDisplayData: updatedHigherPovertyData[0],
+    });
+  };
 
   render() {
-    const { higherPovertyData } = this.state;
+    const { higherPovertyDisplayData, years } = this.state;
     return (
       <div className="BarGraph-sequence-container">
         <div className="BarGraph-container BarGraph-sticky">
           <VictoryChart
             height={400}
             width={400}
-            domainPadding={{ x: 50, y: [0, 20] }}
+            domainPadding={{ x: 50 }}
+            animate={{ duration: 500 }}
           >
-            <VictoryBar data={higherPovertyData} />
+            <VictoryBar data={higherPovertyDisplayData} />
           </VictoryChart>
         </div>
+        {years.map((year) => {
+          return (
+            <>
+              <div className="BubbleGraph-waypoint-buffer" />
+              <Waypoint onEnter={() => this.updateGraph(year)} />
+            </>
+          );
+        })}
       </div>
     );
   }
