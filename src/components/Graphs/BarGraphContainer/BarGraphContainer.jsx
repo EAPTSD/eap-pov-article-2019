f796e810d59b29e4fba22f9b6d083ba53d61422d
deasydoesit <darwin.rinderer@gmail.com>
2019-03-10 18:54:05 -0400

    Configuring BarGraphV2 to function in container

--- src/components/Graphs/BarGraphContainer/BarGraphContainer.jsx
+++ src/components/Graphs/BarGraphContainer/BarGraphContainer.jsx
@@ -7,37 +7,70 @@ import Waypoint from 'react-waypoint';
 // Internal Imports
 import BarGraphV2 from '../BarGraphV2';
 import BarGraphPercentageV2 from '../BarGraphPercentageV2';
+import formatHigherPovertyData from '../../../utilities/formatHigherPovertyData';
 import './BarGraphContainer.css';
+
+// Data
+import higherPovertyData from '../../../data/BarGraphData/EAP_higher_pov.csv';
 
 class BarGraphContainer extends Component {
   state = {
-    data: [],
-    displayText: '',
-    formattedClassData: [],
-    index: 0,
-    percentageData: [],
+    higherPovertyDisplayData: null,
+    reserveData: null,
+    years: [2002, 2006, 2010, 2014, 2018],
+    index: [1, 2, 3, 4, 5],
   };
 
   componentDidMount() {
+    Promise.all([
+      d3.csv(higherPovertyData), //
+    ]).then((files) => {
+      const formattedHigherPovertyData = files.map((file) => {
+        return formatHigherPovertyData(file, 2002);
+      });
+      this.setState({
+        higherPovertyDisplayData: formattedHigherPovertyData[0],
+        reserveData: files,
+      });
+    });
+
     const elements = document.querySelectorAll('.BarGraphContainer-sticky');
     Stickyfill.add(elements);
   }
 
-  updateGraph = () => {};
+  updateGraph = (year) => {
+    const { reserveData } = this.state;
+    const updatedHigherPovertyData = reserveData.map((data) => {
+      return formatHigherPovertyData(data, year);
+    });
+    this.setState({
+      higherPovertyDisplayData: updatedHigherPovertyData[0],
+    });
+  };
 
   render() {
+    const { years, higherPovertyDisplayData } = this.state;
     return (
       <div>
         <div className="BarGraphContainer-sequence-container BarGraphContainer-sticky container-fluid">
           <div className="BarGraphContainer-container row">
             <div className="col-sm">
-              <BarGraphV2 />
+              <BarGraphV2 higherPovertyDisplayData={higherPovertyDisplayData} />
             </div>
             <div className="col-sm">
               <BarGraphPercentageV2 />
             </div>
           </div>
         </div>
+        {years.map((year) => {
+          return (
+            <>
+              <div className="BarGraphContainer-waypoint-buffer" />
+              <Waypoint onEnter={() => this.updateGraph(year)} />
+              <div className="BarGraphContainer-waypoint-buffer" />
+            </>
+          );
+        })}
       </div>
     );
   }
--- src/components/Graphs/BarGraphV2/BarGraphV2.jsx
+++ src/components/Graphs/BarGraphV2/BarGraphV2.jsx
@@ -1,64 +1,37 @@
 // External Imports
-import React, { Component } from 'react';
+import React from 'react';
 import { VictoryChart, VictoryBar } from 'victory';
-import * as d3 from 'd3';
 
 // Internal Imports
-import formatHigherPovertyData from '../../../utilities/formatHigherPovertyData';
 import './BarGraphV2.css';
 
-// Data
-import higherPovertyData from '../../../data/BarGraphData/EAP_higher_pov.csv';
-
-class BarGraphV2 extends Component {
-  state = {
-    higherPovertyDisplayData: null,
-    reserveData: null,
-    years: [2002, 2006, 2010, 2014, 2018],
-  };
-
-  componentDidMount() {
-    Promise.all([
-      d3.csv(higherPovertyData), //
-    ]).then((files) => {
-      const formattedHigherPovertyData = files.map((file) => {
-        return formatHigherPovertyData(file, 2002);
-      });
-      this.setState({
-        higherPovertyDisplayData: formattedHigherPovertyData[0],
-        reserveData: files,
-      });
-    });
-  }
-
-  render() {
-    const { higherPovertyDisplayData } = this.state;
-    return (
-      <div className="BarGraphV2-sequence-container">
-        <div className="BarGraphV2-container">
-          <VictoryChart
-            height={400}
-            width={400}
-            // domain={{ y: [0, 1000] }}
-            domainPadding={{ x: 25 }}
-            animate={{ duration: 500 }}
-          >
-            <VictoryBar
-              style={{
-                data: {
-                  fill: (d) => (d.xName === '< $1.9' ? '#c43a31' : '#000000'),
-                },
-              }}
-              categories={{
-                x: ['< $1.9', '$1.9-$3.2', '$3.2-$5.5', '$5.5-$15', '> $15'],
-              }}
-              data={higherPovertyDisplayData}
-            />
-          </VictoryChart>
-        </div>
+const BarGraphV2 = (props) => {
+  const { higherPovertyDisplayData } = props;
+  return (
+    <div className="BarGraphV2-sequence-container">
+      <div className="BarGraphV2-container">
+        <VictoryChart
+          height={400}
+          width={400}
+          // domain={{ y: [0, 1000] }}
+          domainPadding={{ x: 25 }}
+          animate={{ duration: 500 }}
+        >
+          <VictoryBar
+            style={{
+              data: {
+                fill: (d) => (d.xName === '< $1.9' ? '#c43a31' : '#000000'),
+              },
+            }}
+            categories={{
+              x: ['< $1.9', '$1.9-$3.2', '$3.2-$5.5', '$5.5-$15', '> $15'],
+            }}
+            data={higherPovertyDisplayData}
+          />
+        </VictoryChart>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BarGraphV2;
