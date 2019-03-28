0c40914b23fa99ca3e8c90f90a8e45976c39e9f0
deasydoesit <darwin.rinderer@gmail.com>
2019-03-27 20:06:08 -0400

    Adding rotating bubble graph header text

--- src/components/Graphs/BubbleGraphContainer/BubbleGraphContainer.jsx
+++ src/components/Graphs/BubbleGraphContainer/BubbleGraphContainer.jsx
@@ -1,13 +1,5 @@
 // External Imports
 import React, { Component } from 'react';
-import {
-  VictoryChart,
-  VictoryScatter,
-  VictoryAxis,
-  VictoryLabel,
-  VictoryLegend,
-  VictoryTooltip,
-} from 'victory';
 import * as d3 from 'd3';
 import Waypoint from 'react-waypoint';
 import Stickyfill from 'stickyfilljs';
@@ -19,74 +11,95 @@ import BubbleGraphV2 from '../BubbleGraphV2';
 import './BubbleGraphContainer.css';
 
 // Data
-import asean1_9 from '../../../data/BubbleGraphData/asean_1.9.csv';
-import asean3_2 from '../../../data/BubbleGraphData/asean_3.2.csv';
-import asean5_5 from '../../../data/BubbleGraphData/asean_5.5.csv';
+import asean_wChina_1_9 from '../../../data/BubbleGraphData/asean_wChina_1.9.csv';
+import asean_wChina_3_2 from '../../../data/BubbleGraphData/asean_wChina_3.2.csv';
+import asean_wChina_5_5 from '../../../data/BubbleGraphData/asean_wChina_5.5.csv';
 
 class BubbleGraphContainer extends Component {
   state = {
     displayData: [],
-    reserveData: [],
+    reserveDataWChina: [],
     index: [0, 1, 2, 3, 4, 5, 6, 7],
     colors: ['#191970', '#325DDF', '#87CEFA'],
     asean1_9: null,
     asean3_2: null,
     asean5_5: null,
+    bubbleGraphText: ['with China', 'without China'],
+    displayText: '',
+    headerClass: '',
   };
 
   componentDidMount() {
     const { colors } = this.state;
-    Promise.all([d3.csv(asean1_9), d3.csv(asean3_2), d3.csv(asean5_5)]).then(
-      (files) => {
-        const formattedPovertyData = files.map((file, i) => {
-          const tractColor = colors[i];
-          return formatPovertyData(file, tractColor);
-        });
-        this.setState({
-          reserveData: formattedPovertyData,
-          asean1_9: formattedPovertyData[0],
-          asean3_2: formattedPovertyData[1],
-          asean5_5: formattedPovertyData[2],
-        });
-      }
-    );
+    Promise.all([
+      d3.csv(asean_wChina_1_9),
+      d3.csv(asean_wChina_3_2),
+      d3.csv(asean_wChina_5_5),
+    ]).then((files) => {
+      const formattedPovertyDataWChina = files.map((file, i) => {
+        const tractColor = colors[i];
+        return formatPovertyData(file, tractColor);
+      });
+      this.setState({
+        reserveDataWChina: formattedPovertyDataWChina,
+      });
+    });
 
     const elements = document.querySelectorAll('.BubbleGraphContainer-sticky');
     Stickyfill.add(elements);
   }
 
-  updateGraph = (index) => {
-    const { reserveData } = this.state;
-    const displayArr = displayDataPopulator(reserveData, index);
+  updateGraph = (index, withChina) => {
+    let headerClass;
+    let displayText;
+    const { reserveDataWChina, bubbleGraphText } = this.state;
+    const displayArr = displayDataPopulator(reserveDataWChina, index);
     const res = [];
     for (let i = 0; i < displayArr[0].length; i++) {
       res.push(displayArr[0][i]);
       res.push(displayArr[1][i]);
       res.push(displayArr[2][i]);
     }
+    if (withChina === 'reset') {
+      headerClass = 'fadeOut';
+      displayText = bubbleGraphText[0];
+    } else if (withChina === true) {
+      headerClass = 'fadeIn';
+      displayText = bubbleGraphText[0];
+    } else {
+    }
     this.setState({
       displayData: res,
+      headerClass: headerClass,
+      displayText: displayText,
     });
   };
 
   render() {
-    const { index, displayData } = this.state;
+    const { index, displayData, headerClass, displayText } = this.state;
     const { flowText } = this.props;
     return (
       <div className="BubbleGraphContainer-sequence-container">
-        <h1>hi</h1>
         <div className="BubbleGraphContainer-container BubbleGraphContainer-sticky">
+          <div className="BubbleGraphContainer-header-container text-center">
+            <h1 className="BubbleGraphContainer-header-text">Developing EAP</h1>
+            <span
+              className={`BubbleGraphContainer-header-text-change ${headerClass}`}
+            >
+              {displayText}
+            </span>
+          </div>
           <BubbleGraphV2 displayData={displayData} />
         </div>
         {index.map((i) => {
           return (
             <>
               <div className="BubbleGraphContainer-waypoint-buffer" />
-              {i === 0 ? (
-                <Waypoint onEnter={() => this.updateGraph(0)} />
+              {i === 0 || i === 4 ? (
+                <Waypoint onEnter={() => this.updateGraph(0, 'reset')} />
               ) : null}
               {i === 1 ? (
-                <Waypoint onEnter={() => this.updateGraph(7)} />
+                <Waypoint onEnter={() => this.updateGraph(7, true)} />
               ) : null}
               <div className="BubbleGraphContainer-waypoint-buffer">
                 <p className="bg-text">{flowText[i]}</p>
