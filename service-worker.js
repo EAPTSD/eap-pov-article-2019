472659eac6d98de927dfdfab665051bbe2e70654
deasydoesit <darwin.rinderer@gmail.com>
2019-05-19 19:14:21 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/precache-manifest.f9046480f6c20c39c12aa65746e2d8f0.js"
+  "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/precache-manifest.cb6c65cbc3460fc37fe8e4708cd3a789.js"
 );
 
 workbox.clientsClaim();
