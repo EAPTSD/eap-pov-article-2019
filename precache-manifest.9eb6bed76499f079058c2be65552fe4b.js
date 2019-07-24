47e46e63587013532c6e721f634f65ea365d7f4b
deasydoesit <darwin.rinderer@gmail.com>
2019-07-23 20:42:46 -0400

    Updates

---
+++ precache-manifest.9eb6bed76499f079058c2be65552fe4b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389cc82786f60096791ff184efe0ef0f",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/index.html"
+  },
+  {
+    "revision": "c70f3d190417f03ffd15",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "989b9b7da528c3690323",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/css/main.c98d4651.chunk.css"
+  },
+  {
+    "revision": "c70f3d190417f03ffd15",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/js/2.bf3a69e6.chunk.js"
+  },
+  {
+    "revision": "989b9b7da528c3690323",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/js/main.09f1b7d2.chunk.js"
+  },
+  {
+    "revision": "42f0d994dbe6735cd29d",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/js/runtime~main.7ceefe0d.js"
+  },
+  {
+    "revision": "c04c0e1ba39c576302e4597d6f6fbf6b",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/EAP_higher_pov.c04c0e1b.csv"
+  },
+  {
+    "revision": "5d3ebbf18f46a7b48e1ee110274ffce3",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_wChina_1.9.5d3ebbf1.csv"
+  },
+  {
+    "revision": "b2508c6160ab289da1aa78ed23649518",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_wChina_3.2.b2508c61.csv"
+  },
+  {
+    "revision": "4b1504fd03547e3518a34555755ebc8b",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_wChina_5.5.4b1504fd.csv"
+  },
+  {
+    "revision": "f2dbba84117a2fe4ef00bee86fa78d6f",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_woChina_1.9.f2dbba84.csv"
+  },
+  {
+    "revision": "dc048442fc079e52d7940fae2de30b9c",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_woChina_3.2.dc048442.csv"
+  },
+  {
+    "revision": "578be73b517d2dffcbeb44803dc9be4c",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/asean_woChina_5.5.578be73b.csv"
+  },
+  {
+    "revision": "c1c4a5750576e92634073249b185de83",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/eap_economic_class.c1c4a575.csv"
+  },
+  {
+    "revision": "807a2eb8ffcd2e803f87fbc268ae01f5",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/eap_economic_class_percentage.807a2eb8.csv"
+  },
+  {
+    "revision": "87e5e61b8825f12a7068395dd42d019e",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/eap_ex_china_economic_class.87e5e61b.csv"
+  },
+  {
+    "revision": "40d5b8bad8ac251df466d33f62fbeb4e",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/eap_ex_china_economic_class_percentage.40d5b8ba.csv"
+  },
+  {
+    "revision": "4d58451ecc17a92cd17aef69408152c6",
+    "url": "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/static/media/img1.4d58451e.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/precache-manifest.3c3116b34489291b7826a6d25dbe1408.js"
+  "/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/precache-manifest.9eb6bed76499f079058c2be65552fe4b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/24f402c1-271d-4361-a6b3-2ad3a1fcffe1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
