ad69f5adeaaf36fdeed22b0213f6b4093f96a187
BtheGit <brendan.beltz@mobileposse.com>
2019-08-25 16:15:42 -0400

    First pass of stacked area chart replacing animated bar chart

--- new-version/index.js
+++ new-version/index.js
@@ -81,82 +81,127 @@ window.addEventListener('resize', scroller.resize);
 // d3 Charts
 
 // 1. EAP Poverty By Year - Stacked Area Chart
-const injectionContainer = document.getElementById('EAP_breakdown_yearly');
+// Using this pattern largely: https://medium.com/@louisemoxy/how-to-create-a-stacked-area-chart-with-d3-28a2fee0b8ca
+const renderEAPPovertyChart = async () => {
+  // TODO: Pick colors
+  const colors = [
+    "orangered",
+    "orange",
+    "yellow",
+    "lightgreen",
+    "green",
+  ];
+  const margin = {top: 50, right: 250, bottom: 140, left: 100};
+  const strokeWidth = 1;
 
-// const transformDataforDisplay = (dataset) => {
-//   const totals = dataset[0].map((data, i) => {
-//     return dataset.reduce((memo, curr) => {
-//       return memo + curr[i].y;
-//     }, 0);
-//   });
-//   return dataset.map((data) => {
-//     return data.map((datum, i) => {
-//       return { x: datum.x, y: (datum.y / totals[i]) * 100 };
-//     });
-//   });
-// };
+  const svg = d3
+    .select("#EAP_breakdown_yearly")
+    .append("svg")
+    .attr("height", 600)
+    .attr("width", 1000)
 
-// const formatHigherPovertyDataV2 = (array) => {
-//   const structuredArray = array.map((d, i) => {
-//     const obj = {};
-//     const arrObj = Object.keys(d).map((year, i) => {
-//       return (obj[i] = { x: parseInt(year) });
-//     });
-//     Object.values(d).map((value, i) => {
-//       return (arrObj[i].y = parseFloat(value));
-//     });
-//     return i <= 5 ? arrObj.slice(0, 5) : null;
-//   });
+  const chart = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-//   return transformDataforDisplay(structuredArray);
-// };
+  const width = +svg.attr("width") - margin.left - margin.right - strokeWidth * 2;
+  const height = +svg.attr("height") - margin.top - margin.bottom;
 
-// const extractDataIntoYears = data => {
-//   const { columns, ...rows } = data;
-//   const years = columns.filter(column => column !== 'Pline');
-//   const yearData = {};
-//   const rowsArray = Object.values(rows)
-//   years.map(year => {
-//     yearData[year] = [];
-//     rowsArray.map(row => {
-//       yearData[year].push(row[year])
-//     })
-//   })
-//   return yearData;
-// }
+  const group = chart
+    .append("g")
+    .attr("transform", `translate(-${margin.left - strokeWidth},-${margin.top})`);
 
+  // NOTE: The csv data is incorrectly formatted for our purposes. I manually reformatted some for testing.
+  const data = await d3.csv('./data/EAP_breakdown_yearly/reformatted.csv');
+  // We want all the groups except the year (which is our x axis value)
+  const keys = data.columns.slice(1);
+  const stack = d3.stack().keys(keys);
+  const stackedValues = stack(data);
+  const stackedData = stackedValues.map((layer, index) => {
+    const currentStack = layer.map((d, i) => ({
+        values: d,
+        year: data[i].year
+    }));  
+    return currentStack
+  })
 
-// (function(){
-//   const margin = {top: 10, right: 30, bottom: 0, left: 40},
-//       width = 460 - margin.left - margin.right,
-//       height = 400 - margin.top - margin.bottom;
+  // Create scales
+  // TODO: Ask why the total population counts are the same each year. It looks proportionate, but seems
+  // to claim absolute values.
+  const yScale = d3
+    .scaleLinear()
+    .range([height + margin.top, 0])
+    .domain([0, d3.max(stackedValues[stackedValues.length - 1], dp => dp[1])]);
+  const xScale = d3
+    .scaleLinear()
+    .range([0, width])
+    .domain(d3.extent(data, dataPoint => dataPoint.year));
+
+  const area = d3
+    .area()
+    .x(dataPoint => xScale(dataPoint.year))
+    .y0(dataPoint => yScale(dataPoint.values[0]))
+    .y1(dataPoint => yScale(dataPoint.values[1]));
+
+  const series = group
+    .selectAll(".series")
+    .data(stackedData)
+    .enter()
+    .append("g")
+    .attr("class", "series");
+
+  series
+    .append("path")
+    .attr("transform", `translate(${margin.left},${margin.top})`)
+    .style("fill", (d, i) => colors[i])
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", strokeWidth)
+    .attr("d", d => area(d));
+
+  // Add the X Axis
+  chart
+    .append("g")
+    .attr("transform", `translate(0,${height + margin.top})`)
+    .call(d3.axisBottom(xScale).ticks(data.length, '.4'));
+
+  // Add the Y Axis
+  chart
+    .append("g")
+    .attr("transform", `translate(0, 0)`)
+    .call(d3.axisLeft(yScale));
+
+  // Add y axis label
+  chart
+    .append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(yScale))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -100)
+    .attr("y", -70)
+    .attr("dy", "0.3408em")
+    .attr("fill", "#000")
+    .text("Population (in Millions)")
+    .style("font-size", "25px")
+
+  // Add title text
+  // TODO: Remove this from chart and just add it as a regular element.
+  chart
+    .append("text")
+    .attr("x", 0)
+    .attr("y", -40)
+    .attr("dy", "0.71em")
+    .attr("fill", "#000")
+    .text("The population in developing EAP is shifting towards higher economic classes")
+    .style("font", "23px avenir")
+    .style("fill", "#000000");
   
-//   // append the svg object to the body of the page
-//   const svg = d3.select(".chart-2")
-//     .append("svg")
-//       .attr("width", width + margin.left + margin.right)
-//       .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//       .attr("transform",
-//             "translate(" + margin.left + "," + margin.top + ")");
-  
-            
-//   const chartData = [];
-//   d3.csv('./data/EAP_higher_pov_V2.csv').then(data => {
-//     const extracted = extractDataIntoYears(data);
-//     const columns = extracted['2002'];
+}
 
-//     // append the bar rectangles to the svg element
-//     svg.selectAll("rect")
-//       .data(columns)
-//       .enter()
-//       .append("rect")
-//         .attr("x", (d, i) => i * 21)
-//         .attr("width", 20)
-//         .attr("y", function(d) { return height - ((d / 1000) * height); })
-//         .attr("height", d => {
-//           return (d / 1000) * height;
-//         })
-//         .style("fill", "#69b3a2")
-//   })
-// })()
+// For now, we'll render all charts immediately. If it gets slow we can always lazy render them. (we can also just convert the
+// static charts to images too of course.)
+renderEAPPovertyChart()
+
+// For some reason (TODO: figure it out later) the page refreshes to the middle scroll position.
+// This is a quick hack so I can go back to what I was doing.
+window.scrollTo(0,0);
