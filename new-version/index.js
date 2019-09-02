49c68347615e20ebcfbcb998428b91d62ffdefb6
BtheGit <brendan.beltz@mobileposse.com>
2019-09-02 16:30:43 -0400

    Add plotly chart for option 3 POC

--- new-version/index.js
+++ new-version/index.js
@@ -1,3 +1,5 @@
+const Plotly = window.Plotly;
+
 const navDots = Array.from(document.querySelectorAll('.navigation-dot'));
 
 const getActiveSection = () => {
@@ -30,4 +32,172 @@ window.addEventListener('scroll', scrollHandler);
 scrollHandler();
 // For some reason (TODO: figure it out later) the page refreshes to the middle scroll position.
 // This is a quick hack so I can go back to what I was doing.
-window.scrollTo(0,0);
+// window.scrollTo(0,0);
+
+// #### Plotly chart
+
+const eapBarChartContainer = document.getElementById('eap_bar_chart');
+
+const renderEapBarChart = () => {
+  Plotly.d3.csv('./data/dreaded-bar-anime-data-for-use.csv', rawData => {
+    // The raw data is an array of objects with years, level, and region keys.
+
+    // We want to have a lookup table. First by region, then by year, then by poverty level.
+    // From that we will generate traces for plotly on the fly.
+    const regionalData = rawData.reduce((acc, curr) => {
+      const {
+        Country: region,
+        Threshold: level,
+        ...years
+      } = curr;
+      const regionMapExists = acc.hasOwnProperty(region);
+      if(!regionMapExists){
+        acc[region] = {};
+      }
+      const regionMap = acc[region];
+      Object.entries(years).forEach(([year, population]) => {
+        const yearMapExistsOnRegionMap = regionMap.hasOwnProperty(year);
+        if(!yearMapExistsOnRegionMap){
+          regionMap[year] = {}
+        }
+        const yearMap = regionMap[year];
+        yearMap[level] = population;
+      })
+      return acc
+    }, {})
+    const years = Object.keys(regionalData.China).sort((a,b) => a - b);
+    const traceYear = years[0];
+    const getTrace = (region, year) => {
+      // We're going to need to do this manually so that we can guarantee order. 
+      // NOTE: If the column names change, this will break!
+      const xLabels = [ 
+        'extreme',
+        'moderate',
+        'vulnerable',
+        'secure',
+        'middle_class',
+        'above_mc',
+      ];
+
+      const xLabelFullMap = {
+        'extreme': 'Extremely Poor',
+        'moderate': 'Moderately Poor',
+        'vulnerable': 'Economically Vulnerable',
+        'secure': 'Economically Secure',
+        'middle_class': 'Middle Class',
+        'above_mc': 'Above Middle Class',
+      }
+
+      const regionMap = {
+        'China': {
+          name: 'China',
+          color: '#F18F01',
+        },
+        'RoEAP': {
+          name: 'Rest of developing EAP',
+          color: '#048BA8',
+        }
+      }
+
+      const x = xLabels.map(xLabel => xLabelFullMap[xLabel]);
+      const y = xLabels.map(xLabel => regionalData[region][year][xLabel]);
+
+      return {
+        x,
+        y,
+        name: regionMap[region]['name'],
+        type: 'bar',
+        marker: {
+          color: regionMap[region]['color'],
+          // Uncomment out the following if you want an outline for the bars.
+          // NOTE: you may need to adjust the bargap and bargroupgap to compensate.
+          // line: {
+          //   width: 1,
+          // }
+        },
+      }
+    }
+
+    const getStep = (year) => ({
+      method: 'animate',
+      label: year,
+      args: [
+        year,
+        {
+          mode: 'immediate',
+          transition: {
+            duration: 300,
+          },
+          frame: {
+            duration: 300,
+            redraw: false,
+          },
+        },
+      ],
+    })
+
+    const data = ['RoEAP', 'China'].map(region => getTrace(region, traceYear));
+    const frames = years.map(year => ({
+      name: year,
+      data: ['RoEAP', 'China'].map(region => getTrace(region, year)),
+    }))
+    console.log(frames)
+    const layout = {
+      title: traceYear,
+      xaxis: {
+        tickfont: {
+          size: 14,
+        },
+        automargin: true,
+        fixedrange: true,
+      },
+      yaxis: {
+        title: 'Population',
+        titlefont: {
+          size: 18,
+        },
+        tickfont: {
+          size: 14,
+        },
+        fixedrange: true,
+        range: [0, 800],
+      },
+      barmode: 'group',
+      bargap: 0.25,
+      bargroupgap: .05,
+
+      sliders: [
+        {
+          pad: {
+            l: 0, 
+            t: 100,
+          },
+          currentvalue: {
+            visible: true,
+            prefix: 'Year: ',
+            xanchor: 'right',
+            font: {
+              size: 18, 
+              color: '#666'
+            },
+          },
+          steps: years.map(year => getStep(year)),
+        },
+      ],
+  
+    }
+    
+    Plotly.newPlot('eap_bar_chart', {
+      data,
+      layout,
+      frames,
+      config: {
+        responsive: true,
+        // displaylogo: false,
+        displayModeBar: false,
+      },
+    });
+  })
+}
+
+renderEapBarChart();
