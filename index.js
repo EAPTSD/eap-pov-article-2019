fcf052b59fff53ce8bbad0b92873d082bf955877
BtheGit <brendan.beltz@mobileposse.com>
2019-09-19 16:35:52 -0400

    Add responsive resize handler for charts

--- index.js
+++ index.js
@@ -33,6 +33,35 @@ scrollHandler();
 // This is a quick hack so I can go back to what I was doing.
 // window.scrollTo(0,0);
 
+// https://brendansudol.com/writing/responsive-d3
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width")),
+      height = parseInt(svg.style("height")),
+      aspect = width / height;
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+      .attr("perserveAspectRatio", "xMinYMid")
+      .call(resize);
+
+  // to register multiple listeners for same event type, 
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+      var targetWidth = parseInt(container.style("width"));
+      svg.attr("width", targetWidth);
+      svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
+
+
+
 // #### d3 chart
 const renderEapBarChart = async () => {
   const chartContainerSelector = '#eap_bar_chart';
@@ -161,8 +190,9 @@ const renderEapBarChart = async () => {
     .select(chartContainerSelector)
     .append('svg')
     .attr("width", baseSize.width + baseSize.margin.left + baseSize.margin.right)
-    .attr("height", baseSize.height + baseSize.margin.top + baseSize.margin.bottom);
-
+    .attr("height", baseSize.height + baseSize.margin.top + baseSize.margin.bottom)
+    .call(responsivefy)
+    
   let groupContainer = svg
     .append('g')
     .attr("transform", `translate(${baseSize.margin.left},${baseSize.margin.top})`);
@@ -740,6 +770,7 @@ const renderChoropleth = async () => {
       .attr("height", baseSize.container.height)
       .attr("width", baseSize.container.width)
       .style("background", "black")
+      .call(responsivefy)
   
     const regions = svg
       .append("g")
